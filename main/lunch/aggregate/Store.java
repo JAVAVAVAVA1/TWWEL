a3d0a5eb089d2f90e34876d67b107e4c199faacd
Gombo2 <rhtjddus0502@gmail.com>
2025-02-13 15:38:06 +0900

    Store 데이터 필드 변경 및 메인 실행문 98% 작성완료

--- main/lunch/aggregate/Store.java
+++ main/lunch/aggregate/Store.java
@@ -1,84 +1,49 @@
 package main.lunch.aggregate;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
 
 public class Store implements Serializable {
     private String storeName;           //가게 상호 명
-    private String storeMenu;           //가게 대표 음식
+    private Map<String,Integer> menuCatalogMap;           //가게 메뉴 및 가격
     private String storeLocation;       //가게 위치
-    private int menuPrice;              //음식 가격
     private int maxPeople;              //최대 수용 인원
-    private ClosedDays closedDays;      //휴무일
+    private Set<ClosedDays> closedDays;      //휴무일
     private MenuTag menuTag;            //메뉴 태그 타입
     private StoreStatus storeStatus;    //가게 정보 삭제 유무
 
     public Store() {
     }
 
-    public Store(String storeName, String storeMenu, String storeLocation, int menuPrice, int maxPeople, ClosedDays closedDays, MenuTag menuTag, StoreStatus storeStatus) {
+    public Store(String storeName, Map<String, Integer> menuCatalogMap, String storeLocation,  int maxPeople, Set<ClosedDays> closedDays, MenuTag menuTag, StoreStatus storeStatus) {
         this.storeName = storeName;
-        this.storeMenu = storeMenu;
+        this.menuCatalogMap = menuCatalogMap;
         this.storeLocation = storeLocation;
-        this.menuPrice = menuPrice;
         this.maxPeople = maxPeople;
         this.closedDays = closedDays;
         this.menuTag = menuTag;
         this.storeStatus = storeStatus;
     }
 
-    public String getStoreName() {
-        return storeName;
-    }
-
-    public String getStoreMenu() {
-        return storeMenu;
-    }
-
-    public String getStoreLocation() {
-        return storeLocation;
-    }
-
-    public int getMenuPrice() {
-        return menuPrice;
-    }
-
-    public int getMaxPeople() {
-        return maxPeople;
-    }
-
-    public ClosedDays getClosedDays() {
-        return closedDays;
-    }
-
-    public MenuTag getMenuTag() {
-        return menuTag;
-    }
-
-    public StoreStatus getStoreStatus() {
-        return storeStatus;
-    }
-
     public void setStoreName(String storeName) {
         this.storeName = storeName;
     }
 
-    public void setStoreMenu(String storeMenu) {
-        this.storeMenu = storeMenu;
+    public void setMenuCatalogMap(Map<String, Integer> menuCatalogMap) {
+        this.menuCatalogMap = menuCatalogMap;
     }
 
     public void setStoreLocation(String storeLocation) {
         this.storeLocation = storeLocation;
     }
 
-    public void setMenuPrice(int menuPrice) {
-        this.menuPrice = menuPrice;
-    }
 
     public void setMaxPeople(int maxPeople) {
         this.maxPeople = maxPeople;
     }
 
-    public void setClosedDays(ClosedDays closedDays) {
+    public void setClosedDays(Set<ClosedDays> closedDays) {
         this.closedDays = closedDays;
     }
 
@@ -90,18 +55,47 @@ public class Store implements Serializable {
         this.storeStatus = storeStatus;
     }
 
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public Map<String, Integer> getMenuCatalogMap() {
+        return menuCatalogMap;
+    }
+
+    public String getStoreLocation() {
+        return storeLocation;
+    }
+
+    public int getMaxPeople() {
+        return maxPeople;
+    }
+
+    public Set<ClosedDays> getClosedDays() {
+        return closedDays;
+    }
+
+    public MenuTag getMenuTag() {
+        return menuTag;
+    }
+
+    public StoreStatus getStoreStatus() {
+        return storeStatus;
+    }
+
     @Override
     public String toString() {
         return "Store{" +
                 "storeName='" + storeName + '\'' +
-                ", storeMenu='" + storeMenu + '\'' +
+                ", menuCatalogMap=" + menuCatalogMap +
                 ", storeLocation='" + storeLocation + '\'' +
-                ", menuPrice=" + menuPrice +
                 ", maxPeople=" + maxPeople +
                 ", closedDays=" + closedDays +
                 ", menuTag=" + menuTag +
+                ", storeStatus=" + storeStatus +
                 '}';
     }
+
 
     @Override
     public int hashCode() {
