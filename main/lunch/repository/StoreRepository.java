0edb69d4388f23ac4647cdacabd3f16bf5c172d6
Bluesky3125 <charlie4822@gmail.com>
2025-02-14 17:22:34 +0900

    History 클래스 추가, historyDB 파일 생성 및 로드 기능 추가

--- main/lunch/repository/StoreRepository.java
+++ main/lunch/repository/StoreRepository.java
@@ -1,49 +1,122 @@
 package main.lunch.repository;
 
-import main.lunch.aggregate.StoreStatus;
-import main.lunch.aggregate.ClosedDays;
-import main.lunch.aggregate.MenuTag;
-import main.lunch.aggregate.Store;
+import main.lunch.aggregate.*;
 import main.lunch.stream.MyObjectOutput;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 public class StoreRepository {
-    private final ArrayList<Store> storeList = new ArrayList<>();
+    private final List<Store> storeList = new ArrayList<>();
     private final File file = new File("main/lunch/db/storeDB.dat");
+    private final List<History> historyList = new LinkedList<>();
+    private final File historyFile = new File("main/lunch/db/historyDB.dat");
 
     public StoreRepository() {
         if (!file.exists()) {
-            ArrayList<Store> defaultStores = new ArrayList<>();
+            List<Store> defaultStores = new ArrayList<>();
+            insertDefaultStores((ArrayList<Store>) defaultStores);
+            saveStores((ArrayList<Store>) defaultStores);
 
-            HashMap<String, Integer> defaultCatalog1 = new HashMap<>();
-            HashSet<ClosedDays> defaultClosedDays1 = new HashSet<>();
-            defaultCatalog1.put("떡라면",4500);
-            defaultCatalog1.put("라볶이",5500);
-            defaultClosedDays1.add(ClosedDays.TUE);
-            defaultStores.add(new Store("포마토", defaultCatalog1, "횡단보도 건넌 바로 앞", 6, defaultClosedDays1, MenuTag.SNACK, StoreStatus.OPEN, 1));
-
-            HashMap<String, Integer> defaultCatalog2 = new HashMap<>();
-            HashSet<ClosedDays> defaultClosedDays2 = new HashSet<>();
-            defaultCatalog2.put("햄치즈스페셜",6000);
-            defaultCatalog2.put("햄치즈",5000);
-            defaultClosedDays2.add(ClosedDays.SUN);
-            defaultStores.add(new Store("이삭토스트", defaultCatalog2, "시장 초입", 4, defaultClosedDays2, MenuTag.SANDWICH, StoreStatus.OPEN,2));
-
-            HashMap<String, Integer> defaultCatalog3 = new HashMap<>();
-            HashSet<ClosedDays> defaultClosedDays3 = new HashSet<>();
-            defaultCatalog3.put("프레쉬버거",8400);
-            defaultClosedDays3.clear();
-            defaultClosedDays3.add(ClosedDays.SUN);
-            defaultStores.add(new Store("버거리", defaultCatalog3, "3번 출구 앞", 8, defaultClosedDays3, MenuTag.BURGER, StoreStatus.OPEN,3));
-
-            saveStores(defaultStores);
+            List<History> defaultHistory = new LinkedList<>();
+            insertDefaultHistory((LinkedList<History>) defaultHistory);
+            saveHistory((LinkedList<History>) defaultHistory);
         }
 
         loadStores();
+        loadHistory();
+    }
+
+    private void loadHistory() {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(historyFile)
+                    )
+            );
+            while (true) {
+                historyList.add((History) (ois.readObject()));
+            }
+        } catch (EOFException e) {
+            System.out.println("Loading History Complete!");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void saveHistory(LinkedList<History> defaultHistory) {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(historyFile)
+                    )
+            );
+            for (History history : defaultHistory) {
+                oos.writeObject(history);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void insertDefaultHistory(LinkedList<History> defaultHistory) {
+        HashMap<String, Integer> defaultCatalog1 = new HashMap<>();
+        HashSet<ClosedDays> defaultClosedDays1 = new HashSet<>();
+        defaultCatalog1.put("떡라면", 4500);
+        defaultCatalog1.put("라볶이", 5500);
+        defaultClosedDays1.add(ClosedDays.TUE);
+        defaultHistory.add(new History(LocalDate.of(25, 1, 1),
+                new Store("포마토", defaultCatalog1, "횡단보도 건넌 바로 앞", 6, defaultClosedDays1, MenuTag.SNACK, StoreStatus.OPEN, 1)
+        ));
+    }
+
+    private void insertDefaultStores(ArrayList<Store> defaultStores) {
+        HashMap<String, Integer> defaultCatalog1 = new HashMap<>();
+        HashSet<ClosedDays> defaultClosedDays1 = new HashSet<>();
+        defaultCatalog1.put("떡라면", 4500);
+        defaultCatalog1.put("라볶이", 5500);
+        defaultClosedDays1.add(ClosedDays.TUE);
+        defaultStores.add(new Store("포마토", defaultCatalog1, "횡단보도 건넌 바로 앞", 6, defaultClosedDays1, MenuTag.SNACK, StoreStatus.OPEN, 1));
+
+        HashMap<String, Integer> defaultCatalog2 = new HashMap<>();
+        HashSet<ClosedDays> defaultClosedDays2 = new HashSet<>();
+        defaultCatalog2.put("햄치즈스페셜", 6000);
+        defaultCatalog2.put("햄치즈", 5000);
+        defaultClosedDays2.add(ClosedDays.SUN);
+        defaultStores.add(new Store("이삭토스트", defaultCatalog2, "시장 초입", 4, defaultClosedDays2, MenuTag.SANDWICH, StoreStatus.OPEN, 2));
+
+        HashMap<String, Integer> defaultCatalog3 = new HashMap<>();
+        HashSet<ClosedDays> defaultClosedDays3 = new HashSet<>();
+        defaultCatalog3.put("프레쉬버거", 8400);
+        defaultClosedDays3.add(ClosedDays.SUN);
+        defaultStores.add(new Store("버거리", defaultCatalog3, "3번 출구 앞", 8, defaultClosedDays3, MenuTag.BURGER, StoreStatus.OPEN, 3));
     }
 
     private void loadStores() {
@@ -89,7 +162,6 @@ public class StoreRepository {
                 throw new RuntimeException(e);
             }
         }
-
     }
 
     public ArrayList<Store> selectAllStores() {
@@ -151,7 +223,7 @@ public class StoreRepository {
         for (int i = 0; i < storeList.size(); i++) {
             if (storeList.get(i).getStoreIdx() == reformedStore.getStoreIdx()) {
                 storeList.set(i, reformedStore);
-                saveStores(storeList);
+                saveStores((ArrayList<Store>) storeList);
 
                 return 1;
             }
@@ -166,7 +238,7 @@ public class StoreRepository {
             if (store.getStoreName().equals(removeStoreName)) {
                 store.setStoreStatus(StoreStatus.CLOSED);
                 result = 1;
-                saveStores(storeList);
+                saveStores((ArrayList<Store>) storeList);
             }
         }
         return result;
@@ -176,5 +248,4 @@ public class StoreRepository {
         Store lastStore = storeList.get(storeList.size() - 1);
         return lastStore.getStoreIdx();
     }
-
 }
